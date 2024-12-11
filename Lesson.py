8ea5c1beeb76fb7964d080e9e7f851a473bf9d7d
Anton170791 <levtius@gmail.com>
2024-12-11 13:21:22 +0400

    Add files via upload

---
+++ Lesson.py
@@ -0,0 +1,13 @@
+print(("1st program"))
+print(9**0.5*5)
+print(("2st program"))
+print(99.9>9.98 and 1000!=1000.1)
+print(("3st program"))
+print(2*2+2)
+print(2*(2+2))
+print("str 6" == "str 7")
+print(("4st program"))
+'123.456'
+print(int(float('123.456') * 10) % 10)
+
+
